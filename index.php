d65099519738f14b9a2bd8291483be22875de37d
Patrício Andrade <tricioandrade@outlook.com>
2021-11-10 19:53:10 -0300

    update

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ use app\controller\routes\app as RoutesApp;
 #ini_set('allow_url_include', 'On');
 #ini_set('display_errors',1);
 #error_reporting(E_ALL);
+
 session_start();
         
 const host = 'localhost:3306';
